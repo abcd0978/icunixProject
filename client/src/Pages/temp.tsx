f38ad57b5c7ca97a787883684d48027837393f7a
kimmingyu <kimmin36944@gmail.com>
2023-02-10 04:58:34 +0900

    feat: index Page and main Page(not changed name yet)

---
+++ client/src/Pages/temp.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { useState } from 'react';
+import TreeContainer from '../components/Shapes/TreeContainer';
+export interface ITempProps {
+}
+
+export default function Temp (props: ITempProps) {
+  return (
+  <div>
+    <TreeContainer/>
+  </div>
+  );
+}
