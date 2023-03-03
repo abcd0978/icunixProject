560be3bdde562b58ce9e229fa29a3ab9a3e80547
kimmingyu <kimmin36944@gmail.com>
2023-03-04 01:36:23 +0900

    refactor: reduced codes with great algorithm(Suwon Park did this)

--- client/src/components/Shapes/TreeContainer.tsx
+++ client/src/components/Shapes/TreeContainer.tsx
@@ -7,13 +7,6 @@ import { cNull } from '../../util/cNull';
 export interface ITreeContainerProps {
 }
 
-function ParseNumAndStr(data:any){
-  if(isNaN(data)){
-    return data.length;
-  }
-  return Number(data);
-}
-
 class Node{
   data: string;
   x:number;
@@ -22,7 +15,8 @@ class Node{
   right:Node;
   parent:Node;
   level:number;
-  lines:Line[];
+  gap:number;
+  //lines:Line[];
 
   constructor(data) {
     this.data = data;
@@ -30,7 +24,7 @@ class Node{
     this.right = null;
     this.parent = null;
     this.level = null;
-    this.lines = [];
+    //this.lines = [];
   }
   
   // Set Position
@@ -48,30 +42,33 @@ class Node{
     // return _list;
     return (
       <>
+        {/*Line을 z축의 가장 뒤에 놓기위해서 Line을 먼저 그리기로 했음, 거의 동시에 그려지기 때문에, 그리는 순서에대한 위화감은 안느껴짐*/}
+        {cNull(this.left) !== 0 ? <Line x1={this.x} x2 = {this.left.x} y1 = {this.y} y2 = {this.left.y} r={10} /> : (<></>)}
+        {cNull(this.right) !== 0 ? <Line x1={this.x} x2 = {this.right.x} y1 = {this.y} y2 = {this.right.y} r={10} /> : (<></>)}
+
         <Circle data={this.data} x={this.x} y={this.y} />
         
-        {cNull(this.left) !== 0 ? <Line x1={this.x} x2 = {this.left.x} y1 = {this.y} y2 = {this.left.y}/> : (<></>)}
-        {cNull(this.right) !== 0 ? <Line x1={this.x} x2 = {this.right.x} y1 = {this.y} y2 = {this.right.y}/> : (<></>)}
-
         {cNull(this.left) !== 0 ? this.left.htmlObject() : (<></>)}
         {cNull(this.right) !== 0 ? this.right.htmlObject() : (<></>)}
+
       </>
     )
   }
   
 }
-interface Line{
-  x1:number;
-  y1:number;
-  x2:number;
-  y2:number;
-  r?:number;
-}
+
+// interface Line{
+//   x1:number;
+//   y1:number;
+//   x2:number;
+//   y2:number;
+//   r?:number;
+// }
 
 class binaryTree{
   head:Node;//root node
   nodeArray:Node[] = [];
-  lineArray:Line[] = [];
+  // lineArray:Line[] = [];
   treeLevel:number;
   nodeMap:Map<number,number>;
   newLevelFlag:boolean;
@@ -83,115 +80,57 @@ class binaryTree{
     console.log('생성됨')
   }
 
-  insert_test(node:Node, width:number){
+  /**
+   * @todo: if both node.data and temp.data is number, especially a negative number,
+   *          compare it as number
+   */
+  insert(node:Node){
+    this.newLevelFlag = false;
     if(this.head == null){
       this.head = node;
-      this.head.x = width; // TODO: get width of ViewBox
+      this.head.x = 200; // TODO: get width of ViewBox
       this.head.y = 20;
+      this.head.gap = this.head.x / 2;
       this.head.level = 0;
     } else {
-      
+      let gap:number = 0;
       let temp:Node = this.head;
       while (temp) {
-
-        if (temp.data === node.data) {
-          console.log("중복된 노드값을 넣을 수 없음")
-          break;
+        if ( temp.data == node.data ){
+          console.log("중복된 노드값을 넣을 수 없음");
+          return;
         }
-        if(ParseNumAndStr(temp.data) > ParseNumAndStr(node.data)){
+
+        if(temp.data > node.data){
           if (temp.left == null) {
-            node.parent = temp;
             temp.left = node;
-            temp.left.level = temp.level + 1;
-            let deltaX:number = width / Math.pow(2,temp.left.level);
-            node.x = temp.x - deltaX;
-            node.y = temp.y + 30;
-            if(cNull(this.nodeMap.get(temp.left.level))===0)
-              this.newLevelFlag = true;
-            else
-              this.newLevelFlag = false;
-            let currentLevel = cNull(this.nodeMap.get(temp.left.level))+1;
-            this.nodeMap.set(temp.left.level, currentLevel);
+            gap = -1;
             break;
           } else {
             temp = temp.left;
           }
-        }else if(ParseNumAndStr(temp.data) < ParseNumAndStr(node.data)){
+        } else if(temp.data < node.data){
           if (temp.right == null) {
-            node.parent = temp;
             temp.right = node;
-            temp.right.level = temp.level + 1;
-            let deltaX:number = width / Math.pow(2,temp.right.level);
-            node.x = temp.x + deltaX;
-            node.y = temp.y + 30;
-            if(cNull(this.nodeMap.get(temp.right.level))===0)
-              this.newLevelFlag = true;
-            else
-              this.newLevelFlag = false;
-            let currentLevel = cNull(this.nodeMap.get(temp.right.level))+1;
-            this.nodeMap.set(temp.right.level,  currentLevel);
+            gap = 1;
             break;
           } else {
             temp = temp.right;
           }
         }
       }
-    }
-    return this;
-  }
-
-  insert(node:Node){
-    if(this.head == null){
-      this.head = node;
-      this.head.x = 200; // TODO: get width of ViewBox
-      this.head.y = 20;
-      this.head.level = 0;
-    } else {
       
-      let temp:Node = this.head;
-      while (temp) {
+      node.parent = temp;
 
-        if (temp.data === node.data) {
-          console.log("중복된 노드값을 넣을 수 없음")
-          break;
-        }
-        if(ParseNumAndStr(temp.data) > ParseNumAndStr(node.data)){
-          if (temp.left == null) {
-            node.parent = temp;
-            temp.left = node;
-            temp.left.level = temp.level + 1;
-            let deltaX:number = 200/ Math.pow(2,temp.left.level);
-            node.x = temp.x - deltaX;
-            node.y = temp.y + 30;
-            if(cNull(this.nodeMap.get(temp.left.level))===0)
-              this.newLevelFlag = true;
-            else
-              this.newLevelFlag = false;
-            let currentLevel = cNull(this.nodeMap.get(temp.left.level))+1;
-            this.nodeMap.set(temp.left.level, currentLevel);
-            break;
-          } else {
-            temp = temp.left;
-          }
-        }else if(ParseNumAndStr(temp.data) < ParseNumAndStr(node.data)){
-          if (temp.right == null) {
-            node.parent = temp;
-            temp.right = node;
-            temp.right.level = temp.level + 1;
-            let deltaX:number = 200 / Math.pow(2,temp.right.level);
-            node.x = temp.x + deltaX;
-            node.y = temp.y + 30;
-            if(cNull(this.nodeMap.get(temp.right.level))===0)
-              this.newLevelFlag = true;
-            else
-              this.newLevelFlag = false;
-            let currentLevel = cNull(this.nodeMap.get(temp.right.level))+1;
-            this.nodeMap.set(temp.right.level,  currentLevel);
-            break;
-          } else {
-            temp = temp.right;
-          }
-        }
+      node.level = node.parent.level + 1;
+      node.x = node.parent.x + (gap * node.parent.gap);
+      node.gap = node.parent.gap / 2;
+      node.y = node.parent.y + 30;
+      // check if resetting position of whole tree is needed
+      if ( this.treeLevel < node.level ){
+        //console.log("this.treeLevel("+this.treeLevel+") < node.level("+node.level+") ");
+        this.newLevelFlag = true;
+        this.treeLevel = node.level;
       }
     }
     return this;
@@ -231,7 +170,7 @@ class binaryTree{
     }
     return this
   }
-
+/*
   setLines():void{
     let _line:Line;
     let temp:Node = this.head;
@@ -243,7 +182,7 @@ class binaryTree{
 
         _line.x2 = temp.left.x;
         _line.y2 = temp.left.y;
-        temp.lines.push(_line);
+        //temp.lines.push(_line);
       }
       if(cNull(temp.right) !== 0){
         // draw line from temp to temp.right
@@ -252,32 +191,17 @@ class binaryTree{
         
         _line.x2 = temp.right.x;
         _line.y2 = temp.right.y;
-        temp.lines.push(_line);
+        //temp.lines.push(_line);
       }
     }
   }
-
-  getLevel(node:Node):number{
-    // let result = 0;
-    // while(node){
-    //   node = node.parent;
-    //   result++;
-    // }
-    // return result-1;
-    // --node에 level 속성을 추가하여 미사용 --
-    return node.level;
-  }
-
+*/
   getNodeArray(){
     return this.nodeArray;
   }
 
   getTreeLevel():number{
-    let result:number = 0;
-    for(let i=0;i<20;i++){
-      this.nodeMap.get(i) > 0 ? result++ : result+=0; 
-    }
-    return result;
+    return this.treeLevel;
   }
 
   getNewLevelFlag():boolean{
@@ -294,49 +218,97 @@ class binaryTree{
     //let direction:number = (this.head.x - x/2) > 0 ? 1 : -1 ;
     console.log("head's x position : " + this.head.x)
     this.parallelMove(this.head, different*direction);
-    this.setInterval(this.head,10);
+    // this.setInterval(this.head, 10);
+    this.head.gap = this.head.x / 2;
+    this.recalculate_position(this.head);
   }
 
-  parallelMove(node:Node,interval:number){
-    if(cNull(node)===0)
-    return;
-    let temp:Node = node;
-    if(cNull(temp.left)!==0)
-      temp.left.x = temp.left.x+interval;
-    if(cNull(temp.right)!==0)
-      temp.right.x = temp.right.x+interval;
-    if(cNull(temp.left)===0 && cNull(temp.right)===0)
-      return;
-    this.parallelMove(temp.left,interval);
-    this.parallelMove(temp.right,interval);
-  }
+  /**
+   * @brief X축으로 interval 만큼 주어진 node를 이동
+   * 
+   * @param {Node}node 부모노드 
+   * @param {number}interval X축으로 이동할 거리
+   */
+  parallelMove( node:Node, interval:number ): void {
+    if ( cNull(node) === 0 ) return;
 
-  setInterval(node:Node,interval:number):void{
-    if(cNull(node)===0)
-      return;
-    let temp:Node = node;
-    if(cNull(temp.left)!==0){
-      temp.left.x = temp.left.x-interval;
-      this.setInterval(temp.left,interval);
+    if ( cNull(node.left) !== 0 ) {
+      node.left.x = node.left.x + interval;
+      this.parallelMove( node.left, interval );
     }
-    if(cNull(temp.right)!==0){
-      temp.right.x = temp.right.x+interval;
-      this.setInterval(temp.right,interval);
+
+    if ( cNull(node.right) !== 0 ) {
+      node.right.x = node.right.x + interval;
+      this.parallelMove( node.right, interval );
     }
-    if(cNull(temp.left)===0 && cNull(temp.right)===0)
-      return;
+
   }
   
+  
+  /**
+   * @brief X축으로 left와 right자식 노드에대해 해당 노드의 gap을 더 함(left 자식노드는 - 이동, right 자식노드는 + 이동)
+   * 
+   * @param {Node} node 부모노드 
+   * @param {string=} lastmove 마지막으로 이동한 방향 (미사용)
+   */
+  recalculate_position( node:Node ) : void {
+    console.log( "recalculate_position(" + node.data + ") called" );
+    console.log("node("+ node.data+").gap : " + node.gap);
+    if( cNull(node) === 0 ) return;
+    console.log( node.data+"의 바뀐후 위치:("+node.x+","+node.y+")" );
+
+    if( cNull(node.left)!==0 ){ 
+      console.log( node.left.data+"의 바뀌기전 x좌표:("+node.left.x+`)` );
+      node.left.x = node.x - node.gap;
+      node.left.gap = node.gap / 2;
+      this.recalculate_position( node.left );
+    }
+
+    if( cNull(node.right)!==0 ){
+      console.log( node.right.data+"의 바뀌기전 x좌표:("+node.right.x+`)` );
+      node.right.x = node.x + node.gap;
+      node.right.gap = node.gap / 2;
+      this.recalculate_position( node.right );
+    }
+  }
+  
+  
+  /**
+   * @brief X축으로 left와 right자식 노드에대해 간격을 더 함(left 자식노드는 - 이동, right 자식노드는 + 이동)
+   * 
+   * setInterval(node, width){
+   *    if (left !== null ) { // set left.x -}
+   *    left <-[width/2] - node -[width/2]-> right
+   *    setInterval(node.left, width +- x)
+   *    setInterval(node.right, width +- x)
+   * }
+   * 
+   * @param {Node} node 부모노드 
+   * @param {number} interval X축으로 이동할 거리
+   * @param {string=} lastmove 마지막으로 이동한 방향 (미사용)
+   */
+  setInterval( node:Node, interval:number ) : void {
+    if ( cNull(node) === 0 ) return;
+
+    if ( cNull(node.left) !== 0 ) {
+      node.left.x = node.left.x - interval;
+      this.setInterval( node.left, interval );
+    }
+
+    if ( cNull(node.right) !== 0 ) {
+      node.right.x = node.right.x + interval;
+      this.setInterval( node.right, interval );
+    }
+  }
 }
 
 function func(prev:string, bnt:binaryTree):string{
   
   let treeLevel = bnt.getTreeLevel()-4;//level5부터 늘릴것이기때문에 -4함.
-  console.log("이제 알겠다 너희들의 레벨... : " + treeLevel)
+  let size = 400 + (Math.pow(2, treeLevel)*100); // 400: base size
   if(treeLevel > 0 && bnt.getNewLevelFlag()){
-    console.log("크기변경 조절")
-    let resultString = `0 0 ${400 + (treeLevel*50)} 400`;
-    bnt.setHeadXpos((400 + (treeLevel*50))/2);//root노드를 확대된 viewBox의 중앙으로 옮긴다.
+    let resultString = `0 0 ${size} 400`;
+    bnt.setHeadXpos(size/2); // root노드를 확대된 viewBox의 중앙으로 옮긴다.
     return resultString;
   }else{
     return prev;
@@ -353,14 +325,14 @@ export default function TreeContainer (props: ITreeContainerProps) {
 
   const OnChange = (e:React.ChangeEvent<HTMLInputElement>) =>{
     e.preventDefault();
+    console.log("input: "+ input + ", e.target.value " + e.target.value);
     setInput(e.target.value)
   }
 
   const addNode = () => {
     // setBnt(bnt.insert_test(new Node(input), width).ToArray())
-
     for(let i=0;i<oneNode.length;i++){
-      if(oneNode[i].data === input){
+      if(oneNode[i].data == input){
         alert('중복안돼!');
         return;
       }
@@ -371,8 +343,31 @@ export default function TreeContainer (props: ITreeContainerProps) {
       let result = func(prev,bnt)
       return result;
     });
+    setInput(() => {
+      return "";
+    });
+  }
 
-    // console.log(bnt.htmlObject());
+  const addNodeDebug=()=>{
+
+    setBnt(bnt.insert(new Node(5)).ToArray());
+    setBnt(bnt.insert(new Node(4)).ToArray());
+    setBnt(bnt.insert(new Node(6)).ToArray());
+    setBnt(bnt.insert(new Node(3)).ToArray());
+    setBnt(bnt.insert(new Node(3.5)).ToArray());
+    setBnt(bnt.insert(new Node(4.5)).ToArray());
+    setBnt(bnt.insert(new Node(4.25)).ToArray());
+    setBnt(bnt.insert(new Node(4.75)).ToArray());
+    setBnt(bnt.insert(new Node(7)).ToArray());
+    setBnt(bnt.insert(new Node(5.5)).ToArray());
+    setBnt(bnt.insert(new Node(5.25)).ToArray());
+    setBnt(bnt.insert(new Node(5.75)).ToArray());
+    setBnt(bnt.insert(new Node(6.5)).ToArray());
+    setBnt(bnt.insert(new Node(8)).ToArray());
+    setBnt(bnt.insert(new Node(2)).ToArray());
+
+    setOneNode(bnt.getNodeArray());
+    
   }
 
   useEffect(()=>{
@@ -402,6 +397,10 @@ export default function TreeContainer (props: ITreeContainerProps) {
       addNode
       </button>
       <input onChange={OnChange} type="text" name="nodeData"/>
+
+      <button onClick={addNodeDebug}>
+      addNodeDebug
+      </button>
 
     </div>
   );
